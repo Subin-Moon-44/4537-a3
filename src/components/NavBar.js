e53f8472235b1bc2371b5804241624d272e69891
Subin <smoon44@my.bcit.ca>
2023-04-08 19:48:28 -0700

    Set logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,37 @@
-import { Box, HStack, Heading } from '@chakra-ui/react';
+import { Box, HStack, Heading, Button } from '@chakra-ui/react';
 import React from 'react';
+import axios from 'axios';
 
 function NavBar() {
+    const isAuthed = () => {
+        const appid = localStorage.getItem('appid');
+        return appid && appid.length > 0;
+    }
+
+    const logout = () => {
+        const appid = localStorage.getItem('appid');
+        localStorage.removeItem('appid');
+        axios.post('http://localhost:6061/logout', { appid: appid }).then(() => { });
+        window.location.href = '/login';
+    }
+
     return (
         <Box as="nav" bg="#4488BF" boxShadow="sm" p="5">
             <HStack spacing="10" justify="space-between">
-                <Heading color="#FFE338">
+                <Heading id="logo" color="#FFE338">
                     PokeDéx
                 </Heading>
+                <Button
+                    style={{ visibility: isAuthed ? 'visible' : 'hidden' }}
+                    variant="outline"
+                    color="white"
+                    onClick={() => { logout() }}
+                >
+                    Logout</Button>
             </HStack>
         </Box>
     )
 }
+
 
 export default NavBar
