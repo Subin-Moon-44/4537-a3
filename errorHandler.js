5182e999bd0f595b12de0dbdbf1b932666bbcfce
Subin <smoon44@my.bcit.ca>
2023-04-09 20:40:20 -0700

    update to errorlog

--- errorHandler.js
+++ errorHandler.js
@@ -1,15 +1,27 @@
-const { mongoose } = require('mongoose');
+const { mongoose } = require('mongoose')
+const ErrorLog = require('./errorLog')
 
 handleErr = (err, req, res, next) => {
-    if (err.pokeErrCode) {
-        res.status(err.pokeErrCode);
-    } else {
-        res.status(500);
+    let status = 500
+    if (err.pokeErrCode)
+        status = err.pokeErrCode
+    res.status(status)
+    res.send(err.message)
+    try {
+        const errorLog = new ErrorLog({
+            endpoint: req.url.split('?')[0],
+            errorStatus: status,
+            errorMessage: err.message
+        })
+        errorLog.save()
+    } catch (err) {
+        console.log("Error happening in logging error to db");
+        console.log(err)
     }
-    res.send(err.message);
-    console.log("#####################");
+    console.log("####################")
     console.log(err);
-    console.log("#####################");
+    console.log("####################")
 }
 
-module.exports = { handleErr };
+
+module.exports = { handleErr }
